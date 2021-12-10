8a6cb3067bc2b4b4606a86166de634632d93e25c
n-lundie <noah.lundie@gmail.com>
2021-12-10 22:53:11 +0000

    feat(firestore): installed Firestore library
    Installed Firestore library to enable access to Firestore database.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import { Firestore } from '@google-cloud/firestore';
+
+const db = new Firestore({
+  projectId: 'tildelete-744e4',
+  keyFilename:
+    '/mnt/c/Users/Administrator/Downloads/tildelete-744e4-243bbba415c9.json',
+});
+
+const getStats = async () => {
+  const stats = await db.collection('statistics').doc('overall').get();
+  console.log(stats.data());
+};
+
+getStats();
