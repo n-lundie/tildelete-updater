56fb69e36398e31708da45ddcd9fe7250a407fe4
n-lundie <noah.lundie@gmail.com>
2021-12-24 16:42:28 +0000

    test(validatematch): added negative test conditions to validateMatch test
    Added test to check that validateMatch returns false when given invalid matches.

--- src/tests/validateMatch.test.ts
+++ src/tests/validateMatch.test.ts
@@ -1,9 +1,18 @@
 import validateMatch from '../functions/validateMatch';
-import * as match from './mocks/match-noEd.json';
+import * as matchNoEd from './mocks/match-noEd.json';
+import * as matchBotEd from './mocks/match-bot-Ed.json';
+import * as matchNoPigNoEd from './mocks/match-noPig-noEd.json';
 
 describe('validateMatch', () => {
   test('return true if all filters return true', () => {
-    const res = validateMatch(match);
+    const res = validateMatch(matchNoEd);
     expect(res).toBe(true);
+  });
+
+  test('return false if any filter returns false', () => {
+    const res = validateMatch(matchBotEd);
+    const res2 = validateMatch(matchNoPigNoEd);
+    expect(res).toBe(false);
+    expect(res2).toBe(false);
   });
 });
