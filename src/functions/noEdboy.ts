b368ce058c2fe03133b121c8023d81cbbfcca786
n-lundie <noah.lundie@gmail.com>
2021-12-24 16:02:07 +0000

    test(noedboy): added false test to noEdBoy, fixed jest env variables
    Added test to check noEdBoy returns false when Ed is in game. Updated jest config to use setup files
    to allow access to env variables in tests.

--- src/functions/noEdboy.ts
+++ src/functions/noEdboy.ts
@@ -2,8 +2,8 @@ import { IMatch } from '../interfaces/IMatch';
 
 function noEdboy(match: IMatch): boolean {
   const puuid = process.env.EDBO_PUUID;
-  if (match.metadata.participants.includes(puuid!)) return false;
-  return true;
+
+  return !match.metadata.participants.includes(puuid!);
 }
 
 export default noEdboy;
--- src/tests/noEdboy.test.ts
+++ src/tests/noEdboy.test.ts
@@ -1,9 +1,15 @@
 import noEdboy from '../functions/noEdboy';
-import * as match from './mocks/match.json';
+import * as matchEdYes from './mocks/match.json';
+import * as matchEdNo from './mocks/match-bot-edboy.json';
 
 describe('noEdboy', () => {
-  test('return true if Edboy is not present', () => {
-    const res = noEdboy(match);
+  test('return true if Edboy is NOT present', () => {
+    const res = noEdboy(matchEdYes);
     expect(res).toBe(true);
+  });
+
+  test('return false if Edboy IS present', () => {
+    const res = noEdboy(matchEdNo);
+    expect(res).toBe(false);
   });
 });
